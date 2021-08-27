7d27c3d8f8ac6b49ee1369b547b3dde6109abaad
Cesar Bacca <bacca2095@gmail.com>
2021-08-27 15:34:10 -0500

    commit inicial

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core';
+import { deepOrange } from '@material-ui/core/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: deepOrange,
+  },
+  typography: {
+    fontFamily: "'Nunito', sans-serif",
+  },
+});
