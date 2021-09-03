cb82f432f5ef2f21e68e5099776faae071701ed5
Cesar Bacca <bacca2095@gmail.com>
2021-09-03 16:08:50 -0500

    fix: correccion de adaptador axios

--- src/pages/Career/services/careerService.ts
+++ src/pages/Career/services/careerService.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { CareerModel } from '../models/CareerModel';
 
+axios.defaults.adapter = require('axios/lib/adapters/http');
+
 const instance = axios.create({
   baseURL: 'http://localhost:3001/api',
 });
