a49e039204c1289ef54b58ae25e6d30daa30d0c4
Cesar Bacca <bacca2095@gmail.com>
2021-09-08 21:30:30 -0500

    fix: correccion de unused-imports

--- src/pages/Career/context/CareerContext.tsx
+++ src/pages/Career/context/CareerContext.tsx
@@ -1,11 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { SessionContext } from 'context/SessionContext';
 import * as careerService from '../services/careerService';
 import { CareerModel } from '../models/CareerModel';
